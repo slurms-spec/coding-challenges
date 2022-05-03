b053b05446ee02b0f896a2b83e322f545244ee88
Erin Leigh <adl1790456@gmail.com>
2022-05-03 22:42:41 +0930

    additional kata

--- CC-2022-05-03/main.js
+++ CC-2022-05-03/main.js
@@ -2,8 +2,32 @@
 //  width and the height of the cuboid. Write a function to help Bob with this calculation.
 
 
-class Kata {
-    static getVolumeOfCuboid(length, width, height) {
-        return length * width * height
-    }
+// class Kata {
+//     static getVolumeOfCuboid(length, width, height) {
+//         return length * width * height
+//     }
+// }
+
+
+// Convert number to reversed array of digits
+
+// Given a random non-negative number, you have to return the digits of this number within an array in reverse order.
+
+function digitize(n){
+   
+   // return String(n).split('').map(x).reverse().join('')
+
+
+   let string = String(n)
+    stringSplit = string.split('').reverse().join('')
+    console.log(stringSplit)
+  
+
+
+    //console.log(string)
+
 }
+
+digitize(1234567)
+
+
