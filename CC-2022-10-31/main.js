5082ae9f0d3375223d848a9a28da2d2bef1ca820
Erin Leigh <adl1790456@gmail.com>
2022-11-01 18:01:48 +1030

    oct 31

---
+++ CC-2022-10-31/main.js
@@ -0,0 +1,19 @@
+// Description:
+
+// Given an array of integers, return a new array with each value doubled.
+
+// For example:
+
+// [1, 2, 3] --> [2, 4, 6]
+// Fundamentals
+// Arrays
+
+
+let nums = [1,23,23,4,2,4]
+
+function maps(x){
+    return x.map(e => e*2)
+  
+  }
+
+maps(nums)
